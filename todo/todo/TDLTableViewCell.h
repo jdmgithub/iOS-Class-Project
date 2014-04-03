7ef5612c913c8ab97c325f13dd9b8ff055812075
jdmgithub <jdm_email@yahoo.com>
2014-04-03 10:30:26 -0400

    W1D4

---
+++ todo/todo/TDLTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TDLTableViewCell.h
+//  todo
+//
+//  Created by Jeffery Moulds on 4/3/14.
+//  Copyright (c) 2014 Jeffery Moulds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLTableViewCell : UITableViewCell
+
+@end
---
+++ todo/todo/TDLTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  TDLTableViewCell.m
+//  todo
+//
+//  Created by Jeffery Moulds on 4/3/14.
+//  Copyright (c) 2014 Jeffery Moulds. All rights reserved.
+//
+
+#import "TDLTableViewCell.h"
+
+@implementation TDLTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
